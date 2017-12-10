fa6f015db7e056976348d22311cae9f1bc0fc5c2
pmatejko <matejko.pawel@gmail.com>
2017-12-10 14:58:33 +0100

    Added name of news site to Article

--- fetcher/src/main/java/entities/Article.java
+++ fetcher/src/main/java/entities/Article.java
@@ -4,11 +4,13 @@ import java.time.LocalDate;
 
 public class Article extends NewsPiece {
     private String title;
+    private String newsSite;
 
 
-    public Article(String title, String url, String content, LocalDate timestamp) {
+    public Article(String title, String newsSite, String url, String content, LocalDate timestamp) {
         super(url, content, timestamp);
         this.title = title;
+        this.newsSite = newsSite;
     }
 
 
@@ -18,5 +20,13 @@ public class Article extends NewsPiece {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getNewsSite() {
+        return newsSite;
+    }
+
+    public void setNewsSite(String newsSite) {
+        this.newsSite = newsSite;
     }
 }
