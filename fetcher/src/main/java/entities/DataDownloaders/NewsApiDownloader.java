1a72ae34bb26250fef27c061838fcf490df61ac4
Lukasz <l.uchman@interia.pl>
2017-12-12 20:36:06 +0100

    News getters added.

---
+++ fetcher/src/main/java/entities/DataDownloaders/NewsApiDownloader.java
@@ -0,0 +1,30 @@
+package entities.DataDownloaders;
+
+import java.net.*;
+import java.io.*;
+import javax.json.*;
+
+
+public class NewsApiDownloader implements NewsDownloader {
+
+
+    public JsonObject getNews(String topic) throws IOException{
+
+        URL url = new URL("https://newsapi.org/v2/everything?" +
+                "q=" + topic + "&" +
+                "from=2017-12-11&" +
+                "sortBy=popularity&" +
+                "apiKey=cc36659c4c784994a77282ce4e4dc1ed"
+        );
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+
+        JsonReader reader = Json.createReader(in);
+        JsonObject page = reader.readObject();
+        reader.close();
+        in.close();
+
+        return page;
+    }
+
+}
