a00bfb325a84a25360e0106fc4937010c42037b1
Paweł Matejko <matejko.pawel@gmail.com>
2017-12-20 11:59:17 +0100

    Integrated dataDownloaders package with fetcher.impl one

--- fetcher/src/main/java/fetcher/impl/TwitterAPIFetcher.java
+++ fetcher/src/main/java/fetcher/impl/TwitterAPIFetcher.java
@@ -1,12 +1,16 @@
 package fetcher.impl;
 
-import dataDownloaders.TwitterDownloader;
+import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.builder.api.DefaultApi10a;
+import com.github.scribejava.core.model.*;
+import com.github.scribejava.core.oauth.OAuth10aService;
 import interfaces.Fetcher;
 import model.News;
 import model.Preferences;
 
 import javax.json.*;
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
@@ -17,22 +21,32 @@ import java.util.concurrent.ExecutionException;
 
 
 public class TwitterAPIFetcher implements Fetcher {
-
     private static final String apiUrl = "https://api.twitter.com/1.1/search/tweets.json?q=";
-    private static final TwitterDownloader tweetDownloader = new TwitterDownloader();
+
+    private static OAuth1AccessToken accessToken = null;
+    private final OAuth10aService service = new ServiceBuilder("A3HiwxEgAvPVTfrgrbh759dzj")
+            .apiSecret("yFWPALPeQJsElynktj8SYnfxpWiNZRFegZAJKFAr9TyN3S602D")
+            .build(TwitterApi.instance());
+
 
     @Override
     public List<News> fetch(Preferences preferences) throws IOException, InterruptedException, ExecutionException, ParseException {
         String queryString = buildQueryString(preferences);
 
-        if(!tweetDownloader.haveAccessToken())
-            tweetDownloader.authorize();
+        if(accessToken == null)
+            authorize();
 
-        JsonObject tweets = tweetDownloader.getNews(queryString);
+        final OAuthRequest request = new OAuthRequest(Verb.GET, queryString);
+        service.signRequest(accessToken, request);
+        final Response response = service.execute(request);
 
-        JsonArray jsonArticles = tweets.getJsonArray("statuses");
+        JsonReader reader = Json.createReader(new StringReader(response.getBody()));
+        JsonObject tweets = reader.readObject();
+        reader.close();
 
-        return parseJsonTweetsArray(jsonArticles, preferences);
+        JsonArray jsonTweets = tweets.getJsonArray("statuses");
+
+        return parseJsonTweetsArray(jsonTweets, preferences);
     }
 
     private String buildQueryString(Preferences preferences) throws UnsupportedEncodingException {
@@ -50,6 +64,23 @@ public class TwitterAPIFetcher implements Fetcher {
         return stringBuilder.toString();
     }
 
+    private void authorize() throws IOException, InterruptedException, ExecutionException{
+        final Scanner in = new Scanner(System.in);
+
+        // Obtain the Request Token
+        final OAuth1RequestToken requestToken = service.getRequestToken();
+
+        //to trzeba zmienic, przy autentykacji trzeba przekopiowac kod z neta
+        System.out.println("Now go and authorize ScribeJava here:");
+        System.out.println(service.getAuthorizationUrl(requestToken));
+        System.out.println("And paste the verifier here");
+        System.out.print(">>");
+
+        final String oauthVerifier = in.nextLine();
+
+        // Trade the Request Token and Verfier for the Access Token
+        accessToken = service.getAccessToken(requestToken, oauthVerifier);
+    }
 
     private List<News> parseJsonTweetsArray(JsonArray jsonArticles, Preferences preferences) throws ParseException {
         List<News> tweets = new LinkedList<>();
@@ -69,6 +100,66 @@ public class TwitterAPIFetcher implements Fetcher {
         }
 
         return tweets;
+    }
+
+
+
+    private static class TwitterApi extends DefaultApi10a {
+        private static final String AUTHORIZE_URL = "https://api.twitter.com/oauth/authorize?oauth_token=%s";
+        private static final String REQUEST_TOKEN_RESOURCE = "api.twitter.com/oauth/request_token";
+        private static final String ACCESS_TOKEN_RESOURCE = "api.twitter.com/oauth/access_token";
+
+        protected TwitterApi() {
+        }
+
+        private static class InstanceHolder {
+            private static final TwitterApi INSTANCE = new TwitterApi();
+        }
+
+        public static TwitterApi instance() {
+            return TwitterApi.InstanceHolder.INSTANCE;
+        }
+
+        @Override
+        public String getAccessTokenEndpoint() {
+            return "https://" + ACCESS_TOKEN_RESOURCE;
+        }
+
+        @Override
+        public String getRequestTokenEndpoint() {
+            return "https://" + REQUEST_TOKEN_RESOURCE;
+        }
+
+        @Override
+        public String getAuthorizationUrl(OAuth1RequestToken requestToken) {
+            return String.format(AUTHORIZE_URL, requestToken.getToken());
+        }
+
+        /**
+         * Twitter 'friendlier' authorization endpoint for OAuth.
+         *
+         * Uses SSL.
+         */
+        public static class Authenticate extends TwitterApi {
+
+            private static final String AUTHENTICATE_URL = "https://api.twitter.com/oauth/authenticate?oauth_token=%s";
+
+            private Authenticate() {
+            }
+
+            private static class InstanceHolder {
+                private static final Authenticate INSTANCE = new Authenticate();
+            }
+
+            public static Authenticate instance() {
+                return Authenticate.InstanceHolder.INSTANCE;
+            }
+
+            @Override
+            public String getAuthorizationUrl(OAuth1RequestToken requestToken) {
+                return String.format(AUTHENTICATE_URL, requestToken.getToken());
+            }
+        }
     }
 
 //    public static void main(String args[]) {
@@ -91,6 +182,5 @@ public class TwitterAPIFetcher implements Fetcher {
 //            System.out.println("Exception!");
 //            e.printStackTrace();
 //        }
-//
 //    }
 }
