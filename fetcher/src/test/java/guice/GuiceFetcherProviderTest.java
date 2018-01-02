9490d743033115d6394a998cd639492d7fb1fed3
Lukasz <l.uchman@interia.pl>
2018-01-02 18:50:00 +0100

    Tests v1.1 GuiceFetcherProviderTest change

--- fetcher/src/test/java/guice/GuiceFetcherProviderTest.java
+++ fetcher/src/test/java/guice/GuiceFetcherProviderTest.java
@@ -5,45 +5,56 @@ import com.google.inject.Guice;
 import com.google.inject.Provider;
 import fetcher.impl.NewsAPIFetcher;
 import fetcher.impl.TwitterAPIFetcher;
-import mockit.Mock;
-import mockit.MockUp;
-import mockit.Mocked;
+import interfaces.Fetcher;
+import model.DataProvider;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
 
-public class GuiceFetcherProviderTest {
-    public final class FakeNewsAPIFetcherProvider extends MockUp<Provider<NewsAPIFetcher>> {
-        @Mock
-        public NewsAPIFetcher get() {
-            return new NewsAPIFetcher(null);
+import static org.junit.Assert.assertTrue;
 
-        }
-    }
 
-    public final class FakeTwitterAPIProvider extends MockUp<Provider<TwitterAPIFetcher>> {
-        @Mock
-        public TwitterAPIFetcher get() {
-            return new TwitterAPIFetcher(null);
-        }
-    }
+@RunWith(MockitoJUnitRunner.class)
+public class GuiceFetcherProviderTest  {
 
+    @org.mockito.InjectMocks
     private GuiceFetcherProvider guiceFetcherProvider;
+
+    @org.mockito.Mock
+    private Provider newsAPIFetcherProvider;
+    @org.mockito.Mock
+    private Provider twitterAPIFetcherProvider;
+
 
     @Before
     public void setUp() throws Exception {
-        guiceFetcherProvider = new GuiceFetcherProvider();
+        NewsAPIFetcher newsAPIFetcher = Mockito.mock(NewsAPIFetcher.class);
+        TwitterAPIFetcher twitterAPIFetcher = Mockito.mock(TwitterAPIFetcher.class);
+        Mockito.when(newsAPIFetcherProvider.get()).thenReturn(newsAPIFetcher);
+        Mockito.when(twitterAPIFetcherProvider.get()).thenReturn(twitterAPIFetcher);
+
         Guice.createInjector(new AbstractModule() {
             @Override
             protected void configure() {
                 //bind
             }
         });
+
     }
 
+
     @Test
-    public void getFetcher() throws Exception {
+    public void getFetcherTest() {
+
+        Fetcher newsApiFetcher = guiceFetcherProvider.getFetcher(DataProvider.NEWS_API);
+        assertTrue(newsApiFetcher instanceof NewsAPIFetcher);
+
+        Fetcher twitterFetcher = guiceFetcherProvider.getFetcher(DataProvider.TWITTER_API);
+        assertTrue(twitterFetcher instanceof TwitterAPIFetcher);
+
     }
 
 }
